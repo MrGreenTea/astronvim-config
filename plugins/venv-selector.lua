d89d94119d9f80407e42c76239a7d1376ad04c51
Jonas Bulik <jonas@bulik.dev>
2023-08-08 13:57:37 +0200

    Load virtual env on VeryLazy event

--- plugins/venv-selector.lua
+++ plugins/venv-selector.lua
@@ -1,13 +1,15 @@
 return {
   "linux-cultist/venv-selector.nvim",
-  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
+  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
   opts = {
     poetry_path = "~/.cache/pypoetry/virtualenvs",
+    name = { "venv", ".venv" },
+    dap_enabled = "true",
   },
   init = function()
-    vim.api.nvim_create_autocmd("VimEnter", {
-      desc = "Auto select virtualenv Nvim open",
-      pattern = "*",
+    vim.api.nvim_create_autocmd("User", {
+      desc = "Auto select virtualenv on Nvim open",
+      pattern = "VeryLazy",
       callback = function()
         local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
         if venv ~= "" then require("venv-selector").retrieve_from_cache() end
@@ -15,5 +17,8 @@ return {
       once = true,
     })
   end,
-  event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  -- because of the autocmd this is kinda redundant
+  -- but if there is no cached venv this is needed then
+  cmd = { "VenvSelect", "VenvSelectCached", "VenvSelectCurrent" },
+  ft = { "python" },
 }
