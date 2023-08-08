05c76a36524fa21e3ed7c22af8767e81c23658a6
Jonas Bulik <jonas@bulik.dev>
2023-08-08 13:57:15 +0200

    include treesitter lsp peek

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -2,5 +2,15 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     auto_install = true,
+    textobjects = {
+      lsp_interop = {
+        enable = true,
+        floating_preview_opts = { border = "single" },
+        peek_definition_code = {
+          ["<leader>lp"] = { query = "@function.outer", desc = "Peek function definition" },
+          ["<leader>lP"] = { query = "@class.outer", desc = "Peek class definition" },
+        },
+      },
+    },
   },
 }
