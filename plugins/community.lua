19e3662b5de5c378d2911478c14604af6fb7d0ee
Jonas Bulik <jonas@bulik.dev>
2023-09-19 09:41:51 +0200

    Clean up community plugin configs

--- plugins/community.lua
+++ plugins/community.lua
@@ -2,19 +2,11 @@ return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
   -- Programming languages
+  -- TODO: include them directly and just what I need
   { import = "astrocommunity.pack.rust" },
-  -- { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.svelte" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.lua" },
-
-  -- Completions
-  -- { import = "astrocommunity.completion.codeium-vim" },
-
-  -- Motion
-  -- { import = "astrocommunity.motion.mini-surround" },
-
-  -- Test
 }
