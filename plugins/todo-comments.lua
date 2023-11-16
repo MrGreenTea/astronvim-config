fac2b9e773e0992ac7783ff402fe90ac59a198e1
Jonas Bulik <jonas@bulik.dev>
2023-11-16 11:41:44 +0100

    Add jumping for todos

--- plugins/todo-comments.lua
+++ plugins/todo-comments.lua
@@ -3,4 +3,8 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {},
   event = "User AstroFile",
+  keys = {
+    { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+    { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+  },
 }
